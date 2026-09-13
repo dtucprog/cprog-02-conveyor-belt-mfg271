504e28d5612b82359977aabe425dc0ee50d0749c
Mfg271 <s254523@student.dtu.dk>
2026-09-13 10:07:48 +0200

    Implement capacity check for conveyor belt

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -5,5 +5,11 @@ int MOTOR_CAPACITY = 12;
 int main() {
     int motorCount, totalPackageWeight;
 
-    /// YOUR CODE HERE
+    scanf("%d",&motorCount);
+    scanf("%d",&totalPackageWeight);
+      if (motorCount*MOTOR_CAPACITY>=totalPackageWeight) {
+        printf("Yes! The conveyor belt can carry the packages.\n");
+    } else {
+        printf("No. The conveyor belt cannot carry the packages.\n");
+    }
 }
